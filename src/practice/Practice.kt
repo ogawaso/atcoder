15f97560ec845fb37a2975f31e1cc23031568dfd
Soichiro Ogawa <ogawaso@gmail.com>
2020-06-20 00:04:06 +0900

    practice

---
+++ src/practice/Practice.kt
@@ -0,0 +1,14 @@
+package practice
+
+/**
+ * https://atcoder.jp/contests/practice/tasks/practice_1
+ */
+fun main() {
+   val a = readLine()!!.toInt()
+   val input = readLine()!!.split(" ").map { it.toInt() }
+   val b = input[0]
+   val c = input[1]
+   val s  = readLine()!!
+
+   println("${a + b+ c} $s")
+}
